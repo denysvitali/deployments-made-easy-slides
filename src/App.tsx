5f3890e5a2f0e5c071295fed49ebe38f403e6871
Denys Vitali <denys@denv.it>
2021-10-09 02:09:34 +0200

    feat: add more slides

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Deck, Slide, Heading, Text, Grid, Notes, CodePane, FlexBox, Box } from 'spectacle';
+import { Deck, Slide, Heading, Text, Grid, Notes, CodePane, FlexBox, Box, ListItem, UnorderedList, Progress, FullScreen, SlideTransition } from 'spectacle';
 import tomorrow from 'react-syntax-highlighter/dist/cjs/styles/prism/tomorrow';
 import denvit from './theme/denvit-theme/theme';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
@@ -6,9 +6,43 @@ import {faGithub, faTwitter} from '@fortawesome/free-brands-svg-icons'
 import { faGlobeEurope } from '@fortawesome/free-solid-svg-icons';
 require('typeface-noto-sans');
 require('typeface-source-code-pro');
+
+const template = () => (
+  <FlexBox
+    justifyContent="space-between"
+    position="absolute"
+    bottom={0}
+    width={1}
+  >
+    <Box padding="0 10px">
+      
+    </Box>
+    <Box padding="10px">
+      <Progress color="#ffffffaa" size={10}></Progress>
+    </Box>
+  </FlexBox>
+)
+
+const transition: SlideTransition = {
+  from: {
+    opacity: 0,
+    transform: ''
+  },
+
+  enter: {
+    opacity: 1,
+    transform: '',
+  },
+
+  leave: {
+    opacity: 0,
+    transform: ''
+  }
+}
+
 function App() {
   return (
-    <Deck theme={denvit}>
+    <Deck theme={denvit} template={template} transition={transition}>
       <Slide>
         <FlexBox alignContent="middle" height="100%" flexDirection="column">
           <Heading>Deployments made easy</Heading>
@@ -49,6 +83,105 @@ function App() {
           </FlexBox>
         </Grid>
       </Slide>
+      <Slide 
+        backgroundImage="url(backgrounds/swisscom-building.jpg)"
+        backgroundOpacity={0.2}
+      >
+        <img 
+            src="logos/swisscom-horizontal.svg"
+            height="140"
+            alt="Swisscom Logo"
+          />
+        <FlexBox>
+          <UnorderedList fontSize="40px">
+            <ListItem>~20k employees</ListItem>
+            <ListItem>Not only telecommunications</ListItem>
+            <UnorderedList fontSize="30px">
+              <ListItem>Cloud</ListItem>
+              <ListItem>IoT</ListItem>
+              <ListItem>Security</ListItem>
+              <ListItem>Banking</ListItem>
+              <ListItem>Data Science / Artificial Intelligence</ListItem>
+            </UnorderedList>
+          </UnorderedList>
+        </FlexBox>
+      </Slide>
+
+      <Slide 
+        backgroundImage="url(backgrounds/swisscom-building-2.jpg)"
+        backgroundOpacity={0.2}
+      >
+        <img 
+            src="logos/swisscom-horizontal.svg"
+            height="140"
+            alt="Swisscom Logo"
+          />
+        <FlexBox>
+          <UnorderedList>
+          <ListItem>Multiple locations in Switzerland and abroad</ListItem>
+          <UnorderedList fontSize="30px">
+            <ListItem>Bellinzona, Zurich, Bern, Lausanne, ...</ListItem>
+            <ListItem>Riga (Latvia)</ListItem>
+            <ListItem>Rotterdam (Netherlands)</ListItem>
+            <ListItem>Palo Alto, California (USA)</ListItem>
+            <ListItem>Shangai (China)</ListItem>
+          </UnorderedList>
+          </UnorderedList>
+        </FlexBox>
+      </Slide>
+      <Slide>
+        <img 
+            src="logos/swisscom-horizontal.svg"
+            height="140"
+            alt="Swisscom Logo"
+          />
+        <FlexBox flexDirection="column" height="100%">
+          <Text>We are hiring!</Text>
+          <Text>
+            <a href="https://jobs.swisscom.ch/">jobs.swisscom.ch</a>
+          </Text>
+        </FlexBox>
+      </Slide>
+      <Slide>
+        <Heading>
+            <FlexBox>
+              <Text fontSize="h1">What is</Text>
+              <img 
+                src="logos/waypoint-color-white.svg" 
+                height="120px"
+                alt="Waypoint Logo"
+              />
+              <Text fontSize="h1">?</Text>
+            </FlexBox>
+          </Heading>
+        <UnorderedList>
+          <ListItem>
+            <FlexBox justifyContent="start">
+              <Text className="text-no-padding-left">An</Text>
+              <img 
+                src="logos/hashicorp-logo-white.svg" 
+                height="44"
+                className="hashicorp-inline-logo"
+                alt="Hashicorp Logo"
+              />
+              <Text>product</Text>
+            </FlexBox>
+          </ListItem>
+          <ListItem>
+            Build, Deployment and Release tool
+          </ListItem>
+        </UnorderedList>
+      </Slide>
+      <Slide>
+        <Heading><img 
+            src="logos/waypoint-color-white.svg"
+            height="200"
+            alt="Hashicorp Waypoint Logo"
+          /></Heading>
+        <FlexBox>
+          <Text>A</Text>
+        </FlexBox>
+      </Slide>
       <Slide>
         <Heading>waypoint.hcl</Heading>
         <CodePane language="hcl" theme={tomorrow} highlightRanges={
@@ -71,6 +204,7 @@ function App() {
         `}
         </CodePane>
       </Slide>
+
     </Deck>
   );
 }
--- src/theme/denvit-theme/theme.ts
+++ src/theme/denvit-theme/theme.ts
@@ -3,19 +3,24 @@ import { defaultTheme } from 'spectacle';
 let themeOptions: typeof defaultTheme = {
     colors: {
         primary: '#fff',
-        secondary: 'white',
+        secondary: '#fff',
+        tertiary: "#001155"
     },
     fonts: {
-        header: '"Noto Sans", Helvetica, Arial'
+        header: '"Noto Sans", Helvetica, Arial',
+        text: '"Noto Sans", Helvetica, Arial',
+        monospace: '"Source Code Pro", Menlo, Monaco, Consolas, "Courier New", monospace'
     },
     size: {
         
     },
-    space: [10],
+    space: [16, 24, 32],
     fontSizes: {
         h1: '72px',
         h2: '64px',
         h3: '48px',
+        text: '44px',
+        monospace: '20px',
     }
 };
 
